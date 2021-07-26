2472938457df211effd072cfd963a3a1d15f1c40
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-07-26 16:43:51 +0200

    Add more exercise solutions

--- exercises/option/option3.rs
+++ exercises/option/option3.rs
@@ -1,8 +1,6 @@
 // option3.rs
 // Make me compile! Execute `rustlings hint option3` for hints
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -11,9 +9,10 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => println!("no match"),
+    if let Some(ref p) = y {
+        println!("Co-ordinates are {},{} ", p.x, p.y);
+    } else {
+        println!("no match");
     }
     y; // Fix without deleting this line.
 }
