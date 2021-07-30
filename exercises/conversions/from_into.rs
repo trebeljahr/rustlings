5d4834ff2512358ecf136768b1da03bc4a3127e0
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-07-30 16:59:48 +0200

    Add small exercises to rust

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,19 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s == "" {
+            return Person::default()
+        }
+        let parts : Vec<&str> = s.split(",").collect();
+        if parts[0] == "" || parts.len() != 2 {
+            return Person::default()
+        }
+        match parts[1].parse::<usize>() {
+            Ok(parsedAge) => Person { name: String::from(parts[0]), age: parsedAge },
+            Err(..) => Person::default(),
+        }
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -12,8 +12,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -26,19 +24,40 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from((red, green, blue): (i16, i16, i16)) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: red.try_into()?,
+            green: green.try_into()?,
+            blue: blue.try_into()?,
+        })
+    }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: arr[0].try_into()?,
+            green: arr[1].try_into()?,
+            blue: arr[2].try_into()?,
+        })
+   }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err("Wrong num of items")?
+        }
+        Ok(Color {
+            red: slice[0].try_into()?,
+            green: slice[1].try_into()?,
+            blue: slice[2].try_into()?,
+        }) 
+    }
 }
 
 fn main() {
