474434d4f85766875a4b7a959c2374e984783299
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-08-01 17:42:52 +0200

    Finish rustlings course

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -11,8 +11,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -26,6 +24,17 @@ struct Person {
 impl FromStr for Person {
     type Err = Box<dyn error::Error>;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            Err("Please provide a string")?;
+        }
+        let parts:Vec<&str>= s.split(",").collect();
+        if parts.len() != 2 || parts[0].len() == 0{
+            Err("Format should be Name, Age")?;
+        }
+        Ok(Person {
+            name: String::from(parts[0]),
+            age: parts[1].parse::<usize>()?,
+        })
     }
 }
 
